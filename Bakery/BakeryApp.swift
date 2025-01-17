97b398c6bd2acc11a7e3d9f69c2a64004458c180
Maryam Bahwal <mrim2m7md@gmail.com>
2025-01-17 03:36:56 +0300

    Initial Commit

---
+++ Bakery/BakeryApp.swift
@@ -0,0 +1,17 @@
+//
+//  BakeryApp.swift
+//  Bakery
+//
+//  Created by Maryam Bahwal on 17/07/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct BakeryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
